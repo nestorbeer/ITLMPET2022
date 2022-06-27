9ef24443c15453fa1dc6f4b1217352c19dccc0e4
Nestor Beer <88291173+nestorbeer@users.noreply.github.com>
2022-06-27 07:58:17 -0300

    parcial

--- TPFinal/tp_final/src/App.js
+++ TPFinal/tp_final/src/App.js
@@ -4,15 +4,23 @@ import Footer from './Footer';
 import NavBar from './NavBar';
 import ProductsList from './ProductsList';
 import { Container } from 'react-bootstrap';
+import {BrowserRouter, Route, Routes, useParams} from 'react-router-dom'
+import { useState } from 'react';
 
 function App() {
+  const {categories} = useParams();
+  const [producto, setProducto] = useState()
+  
   return (
-    <Container> 
-      <NavBar></NavBar>
-      <h1>Aca vamos a poner un banner</h1>
-      <h2>Aca vamos a poner un banner mas chiquito</h2>
-      <ProductsList></ProductsList>
-      <Footer></Footer>
+    <Container>
+      <BrowserRouter>
+        <NavBar></NavBar>
+        <Routes>
+          <Route path='/' element={<h1>Aca vamos a poner un banner</h1>}/>
+          <Route path='/categories/:catId' element={<ProductsList id={categories} ></ProductsList>}/>
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
     </Container>
   );
 }
--- TPFinal/tp_final/src/ProductsList.js
+++ TPFinal/tp_final/src/ProductsList.js
@@ -1,15 +1,23 @@
+import { useEffect, useState } from 'react'
 import { Container, ListGroup } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
 import ProductDetail from './ProductDetail'
 import './ProductsList.css'
 
-function ProductsList(){
+function ProductsList(props){
+    const [productos, setProductos] = useState()
+    const {category} = useParams()
+    useEffect(function(){
+        fetch
+        setProductos([])
+    },[category])
     return(
         <Container>
             <ListGroup>
-            <ProductDetail></ProductDetail>
-            <ProductDetail></ProductDetail>
-            <ProductDetail></ProductDetail>
-            <ProductDetail></ProductDetail>
+            <ProductDetail id='1'></ProductDetail>
+            <ProductDetail id={props.id}></ProductDetail>
+            <ProductDetail id={props.id}></ProductDetail>
+            <ProductDetail id={props.id}></ProductDetail>
             </ListGroup>
         </Container>
     )
